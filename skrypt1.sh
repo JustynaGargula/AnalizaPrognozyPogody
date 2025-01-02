08da8f6d1fe11cfa36138df9688233b77274ec77
JustynaGargula <92400632+JustynaGargula@users.noreply.github.com>
2025-01-02 11:05:13 +0100

    Przygotowałam czytanie argumetów i opis w help

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+args=$(getopt -o ":c:hf:" --long "city,help,file" -- "$@")
+if [ $? -ne 0 ]; then
+    echo "Błąd: nieprawidłowe argumenty. Uruchom opcję --help po więcej informacji."
+    exit 1
+fi
+eval set -- "$args"
+
+while true; do
+    case "$1" in
+        --help|-h)
+            cat help1.txt
+            exit 0 ;;
+        -c|--city)
+            city=$2
+            shift ;;
+        -f|--file)
+            fileType=$2
+            shift ;;
+        --)
+            shift
+            break ;;
+        *) 
+            echo "Nieprawidłowy parametr $1"
+            exit 1 ;;
+    esac
+    shift
+done
